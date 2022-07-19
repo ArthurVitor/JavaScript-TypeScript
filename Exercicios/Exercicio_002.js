79993801fcee47ae7dd67e2283ef0d3ebf546752
Arthur <arthurvitorgn@gmail.com>
2022-07-19 18:26:34 -0300

    Const and Let

---
+++ Exercicios/Exercicio_002.js
@@ -0,0 +1,6 @@
+let nome = 'Arthur'
+let idade = 15
+let peso = 84
+let altura = 1.78
+let imc = peso / altura * altura
+console.log(`${nome} tem ${idade} anos de idade pesa ${peso} Kg tem ${altura} portanto tem um IMC de ${imc}`)
