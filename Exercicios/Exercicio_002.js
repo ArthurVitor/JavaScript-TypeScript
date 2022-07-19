357517ab0951cb4105caab321857a3d8679233f0
Arthur <arthurvitorgn@gmail.com>
2022-07-19 19:01:12 -0300

    Tipos primitivos

--- Exercicios/Exercicio_002.js
+++ Exercicios/Exercicio_002.js
@@ -3,4 +3,4 @@ let idade = 15
 let peso = 84
 let altura = 1.78
 let imc = peso / altura * altura
-console.log(`${nome} tem ${idade} anos de idade pesa ${peso} Kg tem ${altura} portanto tem um IMC de ${imc}`)
+console.log(`${nome} tem ${idade} anos de idade pesa ${peso} Kg tem ${altura} portanto tem um IMC de ${imc}`) 
