81ddb4909d5d497ef8351db8bb62ea20ea552f4f
Arthur <arthurvitorgn@gmail.com>
2022-07-21 19:11:48 -0300

    Comparation

---
+++ Aulas/Aula_021/script.js
@@ -0,0 +1,15 @@
+/*
+>Maior
+>Maior ou igual
+<Menor
+<Menor ou igual
+== Igualdade (valor)
+=== Igualdade (valor e tipo)
+!= diferente (valor)
+!== diferente (valor e tipo)
+*/
+
+
+let num1 = 91
+let num2 = '91'
+console.log(num1 === num2)
