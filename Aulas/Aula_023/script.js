9440b97b297c742a5e28d00a5afac20dbd78981e
Arthur <arthurvitorgn@gmail.com>
2022-07-22 16:59:42 -0300

    Exercicio IMC

---
+++ Aulas/Aula_023/script.js
@@ -0,0 +1,13 @@
+const numero = 5;
+
+/*
+if (hora < 12){
+    console.log('Bom dia !')
+}
+*/
+
+if (numero >= 0 && numero <= 5){
+    console.log('O numero está entre 0 e 5')
+} else {
+    console.log('O numero não está entre 0 e 5')
+}
