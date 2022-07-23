5423923bedf57575a663c872813afa6dd9fb8cf2
Arthur <arthurvitorgn@gmail.com>
2022-07-23 14:12:02 -0300

    Atribuição por desestruturação

---
+++ Aulas/Aula_029/script.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: 'Arthur',
+    sobrenome: 'Vitor',
+    idade: 30,
+    endereço: {
+        rua: 'Dominique',
+        numero: 320
+    }
+}    
+const nome = pessoa.nome
+
+console.log(typeof nome);
